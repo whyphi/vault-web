2cb2ad3a2b9c56a8c15e0179a14fed39fe920bb5
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2025-05-22 17:25:58 -0400

    formatted code and fixed .listingId to .id

--- src/app/encrypt-application/page.tsx
+++ src/app/encrypt-application/page.tsx
@@ -1,26 +1,24 @@
-"use client"
+"use client";
 
-import { useAuth } from "../contexts/AuthContext"
-import Loader from "@/components/Loader"
-import { useState, useEffect } from "react"
-import Link from "next/link"
+import { useAuth } from "../contexts/AuthContext";
+import Loader from "@/components/Loader";
+import { useState, useEffect } from "react";
+import { Switch } from "@/components/ui/switch";
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 
-import { Button } from "@/components/ui/button"
-import { Switch } from "@/components/ui/switch"
-
-
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+interface Listing {
+  id: string;
+  title: string;
+  active: boolean;
+  deadline: string;
+  isEncrypted: boolean;
+  dateCreated: string;
+  isVisible: boolean;
+}
 
 export default function Users() {
   const { token } = useAuth();
-  const [listings, setListings] = useState<any[]>([])
+  const [listings, setListings] = useState<Listing[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
@@ -39,40 +37,45 @@ export default function Users() {
   }, []);
 
   if (isLoading) {
-    return <Loader />
+    return <Loader />;
   }
 
   return (
     <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
       <div className="flex flex-col items-start">
-        <h1 className="text-lg font-semibold md:text-2xl">Encrypt Application Data</h1>
+        <h1 className="text-lg font-semibold md:text-2xl">
+          Encrypt Application Data
+        </h1>
         <p className="text-gray-500 mt-2 md:text-sm">
-          Use the toggle switches below to encrypt or decrypt your application data. When encryption is enabled, previous application will be securely hashed and unreadable on the client-side within WhyPhi.
+          Use the toggle switches below to encrypt or decrypt your application
+          data. When encryption is enabled, previous application will be
+          securely hashed and unreadable on the client-side within WhyPhi.
         </p>
       </div>
-      <div
-        className="flex flex-col flex-1 w-full"
-      >
+      <div className="flex flex-col flex-1 w-full">
         {/* Card Component to show Onboarding Stats */}
         <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 lg:gap-4 mb-4">
           {listings.map((listing) => (
-            <Card key={listing.listingId}>
+            <Card key={listing.id}>
               <CardHeader className="flex justify-between">
                 <CardTitle className="text-2xl">{listing.title}</CardTitle>
                 <Switch
                   checked={listing.isEncrypted ?? false}
                   onCheckedChange={async () => {
                     try {
-                      const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/listings/${listing.listingId}/toggle/encryption`, {
-                        method: "PATCH",
-                        headers: {
-                          Authorization: `Bearer ${token}`,
-                        },
-                      });
+                      const response = await fetch(
+                        `${process.env.NEXT_PUBLIC_API_BASE_URL}/listings/${listing.id}/toggle/encryption`,
+                        {
+                          method: "PATCH",
+                          headers: {
+                            Authorization: `Bearer ${token}`,
+                          },
+                        }
+                      );
                       if (response.ok) {
                         setListings((prevListings) =>
                           prevListings.map((item) =>
-                            item.listingId === listing.listingId
+                            item.id === listing.id
                               ? { ...item, isEncrypted: !item.isEncrypted }
                               : item
                           )
@@ -91,5 +94,5 @@ export default function Users() {
         </div>
       </div>
     </main>
-  )
+  );
 }
